cc329a317c6ae5b3927abb03e7ed8c6e12b9105c
Nelson T <128003544+nturcios@users.noreply.github.com>
2023-05-10 19:22:47 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,99 @@
+//Form page JavaScript
+//Saving a refrence for each form element 
+const form = document.getElementById('form'); 
+const fullName = document.getElementById('fullName');
+const email = document.getElementById('email');
+const comment = document.getElementById('comment');
+////Saving a refrence for each form element 
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+    const fullNameValue = fullName.value.trim();
+    const emailValue = email.value.trim();
+    const commentValue = comment.value.trim();
+
+    if (fullNameValue === '') {
+        setError(fullName, 'Full name is required');
+    } else {
+        setSuccess(fullName);
+    }
+
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if (commentValue === '') {
+        setError(comment, 'comment is required');
+    } else if (commentValue.length < 5) {
+        setError(comment, 'You must explain the reason.')
+    } else {
+        setSuccess(comment);
+    }
+};
+
+//JavaScript Overlay menu for my Nav
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+// Home Image Slideshow JavaScript
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
+  
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
